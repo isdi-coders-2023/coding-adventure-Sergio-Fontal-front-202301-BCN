dc9f8d7990f680942f2208600fbf7bfa2913442e
Sergio <sergiot.fontal@gmail.com>
2023-02-28 14:09:21 +0100

    Create a theme with colors and fonts and its structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import GlobalStyles from "./styles/GlobalStyles";
+import theme from "./styles/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  colors: {
+    main: "#cccf18",
+    secondary: "#1e1e1e",
+    terciary: "#f3949a",
+  },
+  fonts: {
+    main: "Saira Stencil One",
+    secondary: "Roboto",
+    terciary: "Faster One",
+  },
+};
+
+export default theme;
