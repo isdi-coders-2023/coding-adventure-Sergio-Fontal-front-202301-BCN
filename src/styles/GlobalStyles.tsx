19c2b53ad4665a5463dfd2aa77b2bb2ea4b4282d
Sergio <sergiot.fontal@gmail.com>
2023-02-28 14:05:10 +0100

    Fix and add elements to GlobalStyles

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -2,14 +2,35 @@ import { createGlobalStyle } from "styled-components";
 import "@fontsource/saira-stencil-one";
 
 const GlobalStyles = createGlobalStyle`
-*, 
-*:after,
-*:before {
+*, *::after, *::before {
+  box-sizing: border-box;
+}
+
+html, body {
+  margin: 0;
+  min-height: 100vh;
+  padding: 0;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+ol, ul, li {
+  list-style: none;
+}
+
+h1, h2, h3, h4, h5, h6 {
   margin: 0;
   padding: 0;
-  box-sizing: border-box;
-  text-decoration: none;
-  }
+  font-size: 1rem;
+}
+
+button {
+  border: none;
+  cursor: pointer;
+}
 `;
 
 export default GlobalStyles;
